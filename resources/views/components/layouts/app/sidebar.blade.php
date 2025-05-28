23cad4363015fcb5c2bbf9103a222ea9a96bf30c
Joe Rucci <rucci.joe@gmail.com>
2025-05-28 15:35:29 -0400

    Add docs

--- resources/views/components/layouts/app/sidebar.blade.php
+++ resources/views/components/layouts/app/sidebar.blade.php
@@ -26,11 +26,13 @@
             <flux:navlist variant="outline">
                 {{-- <flux:navlist.item icon="folder-git-2" href="https://github.com/laravel/livewire-starter-kit" target="_blank">
                 {{ __('Repository') }}
-                </flux:navlist.item>
-
-                <flux:navlist.item icon="book-open-text" href="https://laravel.com/docs/starter-kits#livewire" target="_blank">
-                {{ __('Documentation') }}
                 </flux:navlist.item> --}}
+
+                <flux:navlist.item 
+                    icon="book-open-text" 
+                    href="https://docs.ghostable.dev" target="_blank">
+                {{ __('Documentation') }}
+                </flux:navlist.item>
             </flux:navlist>
             
             <!-- Desktop User Menu -->
