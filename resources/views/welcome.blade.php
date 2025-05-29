0951b7d822d14408a31213a06e935961a08acdcf
Joe Rucci <rucci.joe@gmail.com>
2025-05-29 16:11:58 -0400

    update welcome

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,11 +1,35 @@
 <x-layouts.guest title="Ghostable">
-    <div class="bg-black flex min-h-svh flex-col items-center justify-center gap-6 p-6 md:p-10">
-            <div class="flex w-full max-w-sm flex-col gap-2">
-                <a href="{{ auth()->check() ? route('login') : url('/dashboard') }}" class="flex flex-col items-center gap-2 font-medium" wire:navigate>
-                    <span class="flex h-22 w-22 mb-1 items-center justify-center rounded-md">
-                        <x-app-logo-icon class="size-22 fill-current text-white dark:text-white" />
-                    </span>
-                </a>
+    <flux:main>
+        <div class="py-24 sm:py-32 lg:pb-40">
+            
+        <div class="mx-auto max-w-7xl px-6 lg:px-8">
+          <div class="mx-auto max-w-2xl text-center">
+            <div class="flex flex-col items-center gap-2 font-medium">
+                <span class="flex h-22 w-22 mb-4 items-center justify-center rounded-md">
+                    <x-app-logo-icon class="size-22 fill-current text-black dark:text-white" />
+                </span>
             </div>
+            <h1 class="text-5xl font-semibold tracking-tight text-balance text-black dark:text-white sm:text-7xl">
+                Smart .env Syncing for Laravel Teams
+            </h1>
+            <p class="mt-8 text-lg font-medium text-pretty text-gray-400 sm:text-xl/8">Ghostable makes it easy to securely manage, share, and validate your Laravel environment files—across projects, teams, and pipelines.</p>
+            <div class="mt-10 flex items-center justify-center gap-x-4">
+                <flux:button 
+                    variant="primary" 
+                    href="{{ route('login') }}">
+                    Get Started
+                </flux:button>
+                <flux:button 
+                    variant="ghost" 
+                    target="_blank" 
+                    href="https://docs.ghostable.dev">
+                    Documentation
+                </flux:button>
+            </div>
+          </div>
         </div>
+        
+    </div>
+    </flux:main>
+    
 </x-layouts.guest>
