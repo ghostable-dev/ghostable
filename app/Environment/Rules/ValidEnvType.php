4267dcbd97a7b67f64ef040953c33a1c0848f066
Joe Rucci <rucci.joe@gmail.com>
2025-05-29 14:44:16 -0400

    wip

--- app/Environment/Rules/ValidEnvType.php
+++ app/Environment/Rules/ValidEnvType.php
@@ -2,23 +2,18 @@
 
 namespace App\Environment\Rules;
 
+use App\Environment\Enums\EnvironmentType;
 use Closure;
 use Illuminate\Contracts\Validation\ValidationRule;
 
 class ValidEnvType implements ValidationRule
 {
-    public function __construct(
-        protected array $allowedTypes = []
-    ) {
-        if (empty($this->allowedTypes)) {
-            $this->allowedTypes = config('ghostable.env_types', []);
-        }
-    }
-
     public function validate(string $attribute, mixed $value, Closure $fail): void
     {
-        if (! in_array($value, $this->allowedTypes, strict: true)) {
-            $fail("The selected :attribute [{$value}] is invalid.");
+        $validValues = array_column(EnvironmentType::cases(), 'value');
+
+        if (! is_string($value) || ! in_array($value, $validValues, strict: true)) {
+            $fail("The selected :attribute [{$value}] is not a valid environment type.");
         }
     }
 }
