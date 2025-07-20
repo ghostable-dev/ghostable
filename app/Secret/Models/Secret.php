b1cc1b5e43df80b8791260cf1eaf13a312a75c7b
joerucci <7062653+joerucci@users.noreply.github.com>
2025-07-20 00:03:15 +0000

    fix code style

--- app/Secret/Models/Secret.php
+++ app/Secret/Models/Secret.php
@@ -4,17 +4,17 @@ namespace App\Secret\Models;
 
 use App\Account\Models\User;
 use App\Secret\Actions\LogSecretActivity;
+use App\Secret\Enums\SecretType;
 use App\Secret\Versioning\Actions\CreateSecretVersion;
 use App\Secret\Versioning\Models\SecretVersion;
-use App\Secret\Enums\SecretType;
 use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Concerns\HasUuids;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
-use Illuminate\Database\Eloquent\Relations\MorphTo;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Database\Eloquent\Relations\HasOne;
+use Illuminate\Database\Eloquent\Relations\MorphTo;
 use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Support\Facades\Crypt;
 
