59025726cf45ff4b83f70b759cc917fab96e3f58
Joe Rucci <rucci.joe@gmail.com>
2025-07-20 12:54:47 -0400

    Fix migration morph

--- database/migrations/2025_06_19_000000_create_secrets_table.php
+++ database/migrations/2025_06_19_000000_create_secrets_table.php
@@ -11,7 +11,7 @@ return new class extends Migration
     {
         Schema::create('secrets', function (Blueprint $table) {
             $table->uuid('id')->primary();
-            $table->morphs('owner');
+            $table->uuidMorphs('owner');
             $table->string('name');
             $table->string('type')->default(SecretType::GENERIC->value);
             $table->text('value_encrypted');
