fba2b1bc77cab3a67044fdafd7f691973eea3a75
Joe Rucci <rucci.joe@gmail.com>
2025-07-21 16:50:08 -0400

    Update migrations

--- database/migrations/2025_08_01_000000_add_notifications_columns.php
+++ database/migrations/2025_08_01_000000_add_notifications_columns.php
@@ -9,16 +9,16 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('teams', function (Blueprint $table) {
-            $table->json('notifications')->nullable();
+            $table->json('notifications')->nullable()->after('owner_id');
         });
         Schema::table('projects', function (Blueprint $table) {
-            $table->json('notifications')->nullable();
+            $table->json('notifications')->nullable()->after('team_id');
         });
         Schema::table('environments', function (Blueprint $table) {
-            $table->json('notifications')->nullable();
+            $table->json('notifications')->nullable()->after('file_format');
         });
         Schema::table('secrets', function (Blueprint $table) {
-            $table->json('notifications')->nullable();
+            $table->json('notifications')->nullable()->after('created_by_id');
         });
     }
 
