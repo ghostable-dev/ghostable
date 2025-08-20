b09c2a80a8be88b4b7b44c1ba38e190368b097fe
Joe Rucci <rucci.joe@gmail.com>
2025-08-20 14:20:39 -0400

    Refine TeamProjects messaging and empty state

--- resources/views/project/team-projects.blade.php
+++ resources/views/project/team-projects.blade.php
@@ -1,7 +1,7 @@
 <section class="w-full">
     <div class="relative mb-6 w-full">
-        <flux:heading size="xl" level="1">{{ __('Projects') }}</flux:heading>
-        <flux:subheading size="lg" class="mb-6">{{ __('Manage your environment variables.') }}</flux:subheading>
+        <flux:heading size="xl" level="1">{{ __('All Projects') }}</flux:heading>
+        <flux:subheading size="lg" class="mb-6">{{ __('Browse and manage your team\'s projects.') }}</flux:subheading>
         <flux:separator variant="subtle" />
     </div>
     
@@ -13,25 +13,32 @@
     </flux:modal.trigger>
     @endcan
     
-    <ul role="list" class="grid grid-cols-1 gap-3 sm:grid-cols-2 sm:gap-6 lg:grid-cols-3">
-        @foreach($this->projects as $project)
-            <li class="col-span-1" wire:key="project-{{ $project->id }}">
-                <flux:callout icon="circle-stack">
-                    <flux:callout.heading>
-                        <flux:link href="{{ route('projects.view', $project) }}">{{ $project->name }}</flux:link>
-                    </flux:callout.heading>
-                    <flux:callout.text>
-                        Select an environment from below.
-                    </flux:callout.text>
-                    <x-slot name="actions">
-                        @foreach($project->environments as $env)
-                            <flux:link href="{{ route('environment.variables', $env) }}">{{ $env->name }}</flux:link>
-                        @endforeach
-                    </x-slot>
-                </flux:callout>
-            </li>
-        @endforeach
-    </ul>
+    @if($this->projects->count())
+        <ul role="list" class="grid grid-cols-1 gap-3 sm:grid-cols-2 sm:gap-6 lg:grid-cols-3">
+            @foreach($this->projects as $project)
+                <li class="col-span-1" wire:key="project-{{ $project->id }}">
+                    <flux:callout icon="circle-stack">
+                        <flux:callout.heading>
+                            <flux:link href="{{ route('projects.view', $project) }}">{{ $project->name }}</flux:link>
+                        </flux:callout.heading>
+                        <flux:callout.text>
+                            Select an environment from below.
+                        </flux:callout.text>
+                        <x-slot name="actions">
+                            @foreach($project->environments as $env)
+                                <flux:link href="{{ route('environment.variables', $env) }}">{{ $env->name }}</flux:link>
+                            @endforeach
+                        </x-slot>
+                    </flux:callout>
+                </li>
+            @endforeach
+        </ul>
+    @else
+        <div class="space-y-6">
+            <flux:heading size="md">{{ __('No projects yet') }}</flux:heading>
+            <flux:subheading>{{ __('Get started and create your first project.') }}</flux:subheading>
+        </div>
+    @endif
     
     <livewire:project.livewire.project-create-modal/>
     
