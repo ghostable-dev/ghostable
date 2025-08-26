41b6a1084fde8a66a1ce6431d3702b306fac058c
Joe Rucci <rucci.joe@gmail.com>
2025-08-26 16:01:08 -0400

    refactor: use section component in organization settings

--- resources/views/organization/organization-notifications-manager.blade.php
+++ resources/views/organization/organization-notifications-manager.blade.php
@@ -1,27 +1,27 @@
-<section class="space-y-12 pb-10">
-    
-    <flux:card class="space-y-4 bg-zinc-100/20">
-        <flux:heading size="lg">
+<div class="space-y-6 max-w-4xl">
+
+    <x-section>
+        <x-slot:title>
             <span class="inline-flex items-center gap-2">
                 <flux:icon.slack/>
                 Slack Notifications
             </span>
-        </flux:heading>
-        <flux:subheading>
+        </x-slot:title>
+        <x-slot:subheading>
             Send organization notifications to Slack. All notifications for projects,
             environments, and secrets will also be routed to this endpoint.
-        </flux:subheading>
+        </x-slot:subheading>
         <div>
-            <flux:switch 
-                align="left" 
+            <flux:switch
+                align="left"
                 label="Enabled"
                 wire:model.live="slack_enabled"/>
         </div>
         @if($slack_enabled)
             <form wire:submit="saveSlackSettings" class="space-y-4">
-                <flux:input 
-                    label="Slack Webhook URL" 
-                    type="url" 
+                <flux:input
+                    label="Slack Webhook URL"
+                    type="url"
                     wire:model="slack_webhook_url"/>
                 <div class="flex items-center gap-4">
                     <div class="flex items-center justify-end">
@@ -35,21 +35,28 @@
                 </div>
             </form>
         @endif
-    </flux:card>
-    
-    <flux:fieldset>
-        <div class="space-y-4">
-            @foreach($this->organizationNotificationOptions as $notification)
-                <flux:switch
-                    wire:click="toggle('{{ $notification['key'] }}')"
-                    :checked="$notification['enabled']"
-                    label="{{ $notification['label'] }}"
-                    description="{{ $notification['description'] }}"/>
+    </x-section>
 
-                @if (!$loop->last)
-                    <flux:separator variant="subtle" />
-                @endif
-            @endforeach
-        </div>
-    </flux:fieldset>
-</section>
+    <x-section>
+        <x-slot:title>{{ __('Notifications') }}</x-slot:title>
+        <x-slot:subheading>
+            {{ __('Choose which notifications you\'d like to receive for this organization.') }}
+        </x-slot:subheading>
+        <flux:fieldset>
+            <div class="space-y-4">
+                @foreach($this->organizationNotificationOptions as $notification)
+                    <flux:switch
+                        wire:click="toggle('{{ $notification['key'] }}')"
+                        :checked="$notification['enabled']"
+                        label="{{ $notification['label'] }}"
+                        description="{{ $notification['description'] }}"/>
+
+                    @if (!$loop->last)
+                        <flux:separator variant="subtle" />
+                    @endif
+                @endforeach
+            </div>
+        </flux:fieldset>
+    </x-section>
+
+</div>
