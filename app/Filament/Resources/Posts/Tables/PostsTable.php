d6207c798d03ab2af5f6b2a1317ae6e8c3c2e1fc
Joe Rucci <rucci.joe@gmail.com>
2025-08-27 08:57:04 -0400

    feat: add blog post Filament resource

---
+++ app/Filament/Resources/Posts/Tables/PostsTable.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace App\Filament\Resources\Posts\Tables;
+
+use App\Blog\Enums\PostCategory;
+use App\Blog\Enums\PostStatus;
+use Filament\Actions\BulkActionGroup;
+use Filament\Actions\DeleteBulkAction;
+use Filament\Actions\EditAction;
+use Filament\Tables\Columns\IconColumn;
+use Filament\Tables\Columns\TextColumn;
+use Filament\Tables\Table;
+
+class PostsTable
+{
+    public static function configure(Table $table): Table
+    {
+        return $table
+            ->columns([
+                TextColumn::make('id')
+                    ->label('ID')
+                    ->searchable(),
+                TextColumn::make('title')
+                    ->searchable(),
+                TextColumn::make('slug')
+                    ->searchable(),
+                TextColumn::make('category')
+                    ->formatStateUsing(fn (PostCategory $state) => $state->label())
+                    ->badge(),
+                TextColumn::make('status')
+                    ->formatStateUsing(fn (PostStatus $state) => $state->label())
+                    ->badge(),
+                IconColumn::make('is_featured')
+                    ->boolean(),
+                TextColumn::make('posted_at')
+                    ->dateTime()
+                    ->sortable(),
+                TextColumn::make('created_at')
+                    ->dateTime()
+                    ->sortable()
+                    ->toggleable(isToggledHiddenByDefault: true),
+                TextColumn::make('updated_at')
+                    ->dateTime()
+                    ->sortable()
+                    ->toggleable(isToggledHiddenByDefault: true),
+            ])
+            ->filters([
+                //
+            ])
+            ->recordActions([
+                EditAction::make(),
+            ])
+            ->toolbarActions([
+                BulkActionGroup::make([
+                    DeleteBulkAction::make(),
+                ]),
+            ]);
+    }
+}
