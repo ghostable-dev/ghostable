58b842e5a35fb2ccab3fc1f2e97071d7f5dc13af
Joe Rucci <rucci.joe@gmail.com>
2025-08-28 12:01:09 -0400

    refactor: update plan feature tests

--- tests/Feature/OrganizationFeatureGatesTest.php
+++ tests/Feature/OrganizationFeatureGatesTest.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Organization\Actions\CreateOrganization;
+use App\Organization\Entities\OrganizationFeatures;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Support\Facades\Gate;
 use Laravel\Cashier\Subscription;
@@ -23,7 +24,7 @@ test('personal organization cannot view audit logs by default', function () {
 
 test('audit feature can be enabled for personal organization', function () {
     $organization = app(CreateOrganization::class)->handle('Personal', $this->owner);
-    $organization->update(['features' => ['audits' => true]]);
+    $organization->update(['features' => OrganizationFeatures::from(['audits' => true])]);
 
     expect(Gate::forUser($this->owner)->allows('viewAuditLogs', $organization))->toBeTrue();
 });
@@ -36,7 +37,7 @@ test('personal organization cannot manage access controls by default', function 
 
 test('advanced permissions feature can be enabled for personal organization', function () {
     $organization = app(CreateOrganization::class)->handle('Personal', $this->owner);
-    $organization->update(['features' => ['advanced_permissions' => true]]);
+    $organization->update(['features' => OrganizationFeatures::from(['advanced_permissions' => true])]);
     $organization = $organization->fresh();
     $organization = Mockery::mock($organization)->makePartial();
     $organization->shouldReceive('activeSubscription')->andReturn(Mockery::mock(Subscription::class));
