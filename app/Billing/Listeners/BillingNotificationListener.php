9bb504c0edb1e35dc957b9ce99359dbbff985b90
Joe Rucci <rucci.joe@gmail.com>
2025-08-28 15:08:20 -0400

    wip

--- app/Billing/Listeners/BillingNotificationListener.php
+++ app/Billing/Listeners/BillingNotificationListener.php
@@ -2,8 +2,6 @@
 
 namespace App\Billing\Listeners;
 
-use App\Account\Managers\ACLManager;
-use App\Account\Providers\ACLServiceProvider;
 use App\Organization\Models\Organization;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Support\Collection;
@@ -12,12 +10,6 @@ abstract class BillingNotificationListener implements ShouldQueue
 {
     protected function notifiables(Organization $organization): Collection
     {
-        return collect();
-        // return $organization->users()
-        //     ->withRolesInAccount(
-        //         $account,
-        //         ACLManager::getRole(ACLServiceProvider::ROLE_ADMIN),
-        //         ACLManager::getRole(ACLServiceProvider::ROLE_BILLING_MANAGER)
-        //     )->get();
+        return $organization->users;
     }
 }
