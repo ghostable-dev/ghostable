57b33b3656551c4e8baaffc8e265b39d4136607a
joerucci <7062653+joerucci@users.noreply.github.com>
2025-09-02 18:19:21 +0000

    fix code style

--- tests/Unit/Blog/Markdown/CustomConverterTest.php
+++ tests/Unit/Blog/Markdown/CustomConverterTest.php
@@ -3,7 +3,7 @@
 use App\Blog\Markdown\CustomConverter;
 
 it('converts markdown with custom extensions', function () {
-    $converter = new CustomConverter();
-    $html = (string) $converter->convert("~~strike~~");
+    $converter = new CustomConverter;
+    $html = (string) $converter->convert('~~strike~~');
     expect($html)->toContain('<del>strike</del>');
 });
--- tests/Unit/Blog/Markdown/YouTubeEmbedParserTest.php
+++ tests/Unit/Blog/Markdown/YouTubeEmbedParserTest.php
@@ -3,7 +3,7 @@
 use App\Blog\Markdown\CustomConverter;
 
 it('converts youtube embeds into iframes', function () {
-    $converter = new CustomConverter();
+    $converter = new CustomConverter;
     $html = (string) $converter->convert('@[youtube](https://example.com/embed/abc)');
     expect($html)->toContain('<iframe');
 });
