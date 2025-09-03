71845a6d746a225f0e4fa58902ff00abb99e4973
Joe Rucci <rucci.joe@gmail.com>
2025-09-03 12:53:50 -0400

    Blog design tweaks

--- resources/views/blog/livewire/post-show.blade.php
+++ resources/views/blog/livewire/post-show.blade.php
@@ -10,22 +10,10 @@
 @endpush
 
 <div class="px-6 lg:px-8 bg-white py-10">
-    <div class="mx-auto max-w-2xl lg:max-w-7xl">
-        <h2 class="mt-16 font-mono text-xs/5 font-semibold tracking-widest text-gray-500 uppercase">
-            {{ $post->posted_at->isoFormat('dddd, MMMM D, YYYY') }}
-        </h2>
-
-        <h1 class="mt-2 text-4xl font-medium tracking-tighter text-pretty text-gray-950 sm:text-6xl">
-            {{ $post->title }}
-        </h1>
-
-        <div class="mt-16 grid grid-cols-1 gap-8 pb-24 lg:grid-cols-[15rem_1fr] xl:grid-cols-[15rem_1fr_15rem]">
+    <div class="mt-4 mx-auto max-w-2xl lg:max-w-7xl">
+       
+        <div class="grid grid-cols-1 gap-8 pb-24 lg:grid-cols-[15rem_1fr] xl:grid-cols-[15rem_1fr_15rem]">
             <div class="flex flex-wrap items-center gap-8 max-lg:justify-between lg:flex-col lg:items-start">
-                {{-- <div class="flex items-center gap-3">
-                    <img alt="" class="aspect-square size-6 rounded-full object-cover" src="{{ asset('images/crypto-dark.svg') }}">
-                    <div class="text-sm/5 text-gray-700">Ghostable Team</div>
-                </div> --}}
-
                 <div class="flex flex-wrap gap-2">
                     <a
                         class="rounded-full border border-dotted border-gray-300 bg-gray-50 px-2 text-sm/6 font-medium text-gray-500"
@@ -37,17 +25,27 @@
 
             <div class="text-gray-700">
                 <div class="max-w-2xl xl:mx-auto">
+                    
+                    <div class="mb-10">
+                        <h2 class="font-mono text-xs/5 font-semibold tracking-widest text-gray-500 uppercase">
+                            {{ $post->posted_at->isoFormat('dddd, MMMM D, YYYY') }}
+                        </h2>
+                        <h1 class="mt-2 text-4xl font-medium tracking-tighter text-pretty text-gray-950 sm:text-5xl">
+                            {{ $post->title }}
+                        </h1>
+                    </div>
+                    
+                    @if($post->description)
+                        <p class="my-10 text-xl text-pretty text-base/8 first:mt-0 last:mb-0">
+                            {{ $post->description }}
+                        </p>
+                    @endif
+                    
                     @if($post->hero)
                         <img
                             alt="{{ $post->title }}"
                             class="mb-10 aspect-3/2 w-full rounded-2xl object-cover shadow-xl"
                             src="{{ asset($post->hero) }}">
-                    @endif
-
-                    @if($post->description)
-                        <p class="my-10 text-base/8 first:mt-0 last:mb-0">
-                            {{ $post->description }}
-                        </p>
                     @endif
 
                     <div class="prose my-10 max-w-2xl">
