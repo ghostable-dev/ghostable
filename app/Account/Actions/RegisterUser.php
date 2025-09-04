f6993f988e8bd441d2adde5d56fb4b754e0c0375
Joe Rucci <rucci.joe@gmail.com>
2025-09-04 12:46:19 -0400

    Remove double hashing
    Addresses issue with users not being able to login after signup

--- app/Account/Actions/RegisterUser.php
+++ app/Account/Actions/RegisterUser.php
@@ -4,15 +4,12 @@ namespace App\Account\Actions;
 
 use App\Account\Models\User;
 use Illuminate\Auth\Events\Registered;
-use Illuminate\Support\Facades\Hash;
 
 class RegisterUser
 {
     public function handle(array $data): User
     {
         $data['email'] = strtolower($data['email']);
-
-        $data['password'] = Hash::make($data['password']);
 
         $user = User::create($data);
 
