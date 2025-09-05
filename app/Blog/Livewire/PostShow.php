cfc746d6155b0f85d04660bda13b9c1888b8f6f6
joerucci <7062653+joerucci@users.noreply.github.com>
2025-09-05 13:41:49 +0000

    fix code style

--- app/Blog/Livewire/PostShow.php
+++ app/Blog/Livewire/PostShow.php
@@ -4,14 +4,13 @@ namespace App\Blog\Livewire;
 
 use App\Blog\Models\Post;
 use Livewire\Attributes\Layout;
-use Livewire\Attributes\Title;
 use Livewire\Component;
 
 #[Layout('components.layouts.blog')]
 class PostShow extends Component
 {
     public Post $post;
-    
+
     public function render()
     {
         return view('blog.livewire.post-show')
