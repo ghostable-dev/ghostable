7fc6f881f2cc580eaf3fced21bf1652831336e50
Joe Rucci <rucci.joe@gmail.com>
2025-09-16 16:19:30 -0400

    Add CDN helper

---
+++ bootstrap/mail_asset.php
@@ -0,0 +1,8 @@
+<?php
+
+if (! function_exists('mail_asset')) {
+    function mail_asset(string $path): string
+    {
+        return rtrim(config('mail.cdn'), '/') . '/' . ltrim($path, '/');
+    }
+}
