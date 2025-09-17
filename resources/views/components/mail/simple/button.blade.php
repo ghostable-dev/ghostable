7c5e9930074f87d4574a4e293f988d77f43dca66
Joe Rucci <rucci.joe@gmail.com>
2025-09-16 21:22:41 -0400

    wip

--- resources/views/components/mail/simple/button.blade.php
+++ resources/views/components/mail/simple/button.blade.php
@@ -1,13 +1,11 @@
 @props([
     'href',
-    'color' => "color: #ffffff; color:var(--btn-text, #ffffff);",
-    'background' => "background-color: #080808; background-color:var(--btn-bg, #080808); background: #080808; background:var(--btn-bg, #080808);"
 ])
 <table role="presentation" cellpadding="0" cellspacing="0" border="0" style="margin:0 0 40px;width:100%;">
     <tr>
         <td align="center">
             <a href="{{ $href }}"
-                style="{{ $color }}{{ $background }}display:inline-block;text-decoration:none;padding:16px 48px;font-size:16px;font-weight:600;border-radius:22px;font-family:-apple-system,BlinkMacSystemFont,'Albert Sans',Roboto,Oxygen,Ubuntu,Cantarell,'Fira Sans','Droid Sans','Helvetica Neue',sans-serif;">
+                style="color:#ffffff;color:var(--btn-text,#ffffff);background-color:#080808;background-color:var(--btn-bg,#080808);background:#080808;background:var(--btn-bg,#080808);display:inline-block;text-decoration:none;padding:16px 48px;font-size:16px;font-weight:600;border-radius:22px;font-family:-apple-system,BlinkMacSystemFont,'Albert Sans',Roboto,Oxygen,Ubuntu,Cantarell,'Fira Sans','Droid Sans','Helvetica Neue',sans-serif;">
                 <span><!--[if mso]><i style="mso-font-width:480%;mso-text-raise:24" hidden>&#8202;&#8202;&#8202;&#8202;&#8202;</i><![endif]--></span>
                 <span style="line-height:120%;mso-text-raise:12px;">
                     {{ $slot }}
