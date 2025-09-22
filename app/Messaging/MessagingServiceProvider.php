8a6d5f819629d03ce088edd72ae859e06cd188c5
Joe Rucci <rucci.joe@gmail.com>
2025-09-22 17:39:40 -0400

    Update rate limiter

--- app/Messaging/MessagingServiceProvider.php
+++ app/Messaging/MessagingServiceProvider.php
@@ -46,7 +46,8 @@ class MessagingServiceProvider extends ServiceProvider
     public function boot(): void
     {
         RateLimiter::for(self::MAIL_GLOBAL_LIMITER, fn () => [
-            Limit::perMinute(100)->by(self::MAIL_GLOBAL_LIMITER),
+            Limit::perSecond(1)->by(self::MAIL_GLOBAL_LIMITER.':sec'),
+            Limit::perMinute(90)->by(self::MAIL_GLOBAL_LIMITER.':min'),
         ]);
 
         Event::listen(MessageSent::class, MarkMessageAsSent::class);
