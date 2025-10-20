f7a25c03b58a57ecc5ebd31d7cd6209e7fdf4b18
joerucci <7062653+joerucci@users.noreply.github.com>
2025-10-20 20:36:05 +0000

    fix code style

--- tests/Feature/Environment/DeleteEnvironmentTest.php
+++ tests/Feature/Environment/DeleteEnvironmentTest.php
@@ -1,10 +1,5 @@
 <?php
 
-use App\Environment\Enums\EnvironmentType;
-use App\Environment\Livewire\EnvironmentGeneralSettings;
-use App\Environment\Variable\Actions\CreateVariable;
-use App\Environment\Variable\Entities\CreateVariableData;
 use Illuminate\Foundation\Testing\RefreshDatabase;
-use Livewire\Livewire;
 
 uses(RefreshDatabase::class);
