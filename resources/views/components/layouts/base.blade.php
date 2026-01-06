8ddc8f82a5ffe8ab16cdd928def28e6e901f26f2
Joe Rucci <rucci.joe@gmail.com>
2026-01-06 10:43:14 -0500

    Reorder icons

--- resources/views/components/layouts/base.blade.php
+++ resources/views/components/layouts/base.blade.php
@@ -24,8 +24,8 @@
     
     @stack('meta')
     
-    <link rel="icon" type="image/png" href="/favicon-96x96.png" sizes="96x96" />
     <link rel="icon" type="image/svg+xml" href="/favicon.svg" />
+    <link rel="icon" type="image/png" href="/favicon-96x96.png" sizes="96x96" />
     <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
     <link rel="manifest" href="/site.webmanifest" />
     <link rel="preconnect" href="https://fonts.bunny.net">
